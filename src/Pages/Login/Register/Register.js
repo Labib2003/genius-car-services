79f064b47bb9882709b58b42be6482c207ac4bf9
Labib2003 <labib.salimi30@gmail.com>
2022-04-13 19:17:36 +0600

    register form made with vanilla css

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Register.css';
 
 const Register = () => {
+    const navigate = useNavigate();
+    
+    const navigateLogin = () => {
+        navigate('/login');
+    }
+
+    const handleRegister = (event) => {
+        event.preventDefault();
+    }
+
     return (
-        <div>
+        <div className='register-form'>
             <h2>Please Register</h2>
+            <form onSubmit={handleRegister}>
+                <input type="text" name='name' placeholder='Your name' />
+                <input type="email" name="email" placeholder='Email address' required />
+                <input type="password" name="password" placeholder='Password' required />
+                <input type="submit" value="Register" />
+            </form>
+            <p>Already have an account? <Link to='/login' onClick={navigateLogin} className='text-danger text-decoration-none'>Login</Link></p>
         </div>
     );
 };
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -2,11 +2,9 @@ import React from 'react';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
-import auth from '../../../firebase.init';
 import logo from '../../../images/logo.png'
 
 const Header = () => {
-    const [user] = useAuthState(auth);
     return (
         <header>
             <Navbar collapseOnSelect expand="lg" sticky='top' bg="primary" variant="dark">
